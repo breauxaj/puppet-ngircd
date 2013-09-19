831f9eff3b2f73cafeeb3a2c950350e7442dfa10
Adam Breaux <breauxaj@gmail.com>
2013-09-19 10:41:56 -0400

    wip

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,3 +33,36 @@ define ngircd::service ( $ensure = running,
   }
 
 }
+
+define ngircd::config ( $global_name,
+                        $global_info,
+                        $global_password,
+                        $global_admininfo1,
+                        $global_admininfo2,
+                        $global_adminemail,
+                        $global_ports,
+                        $global_listen,
+                        $global_motdfile,
+                        $global_motdphrase,
+                        $global_serveruid,
+                        $global_servergid,
+                        $global_chrootdir,
+                        $global_pidfile,
+                        $global_pingtimeout,
+                        $global_pongtimeout,
+                        $global_connectretry,
+                        $global_opercanusemode,
+                        $global_operservermode,
+                        $global_predefchannelsonly,
+                        $global_nodns,
+                        $global_connectipv6,
+                        $global_connectipv4,       
+                        $global_maxconnections,
+                        $global_maxconnectionsip,
+                        $global_maxjoins,
+                        $global_maxnicklength,
+
+
+) {
+
+}
